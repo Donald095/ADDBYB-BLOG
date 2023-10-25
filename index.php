a6888bbcfa7d51afea9e8646faa8f42fb3aa6d79
Donald Singh <donaldsingh095@gmail.com>
2023-10-25 19:14:41 +0530

    done -new

--- index.php
+++ index.php
@@ -812,17 +812,17 @@
             <!-- Newsletter Start -->
             <div class="pb-3">
                <div class="bg-light py-2 px-4 mb-3">
-                  <h3 class="m-0">Newsletter</h3>
+                  <h3 class="m-0">NEWSLETTER SIGNUP</h3>
                </div>
                <div class="bg-light text-center p-4 mb-3">
-                  <p>Aliqu justo et labore at eirmod justo sea erat diam dolor diam vero kasd</p>
+                  <p>And stay informed about our blogs</p>
                   <div class="input-group" style="width: 100%;">
                      <input type="text" class="form-control form-control-lg" placeholder="Your Email">
                      <div class="input-group-append">
                         <button class="btn btn-primary">Sign Up</button>
                      </div>
                   </div>
-                  <small>Sign Up and Subscribe</small>
+                  <!-- <small>Sign Up and Subscribe</small> -->
                </div>
             </div>
             <!-- Newsletter End -->
